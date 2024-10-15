3cc9e7595767b97926707fa072f4ee46733e2271
PCuongVN <45710344+PCuongVN101@users.noreply.github.com>
2024-10-15 23:42:44 +0700

    Add files via upload

---
+++ Assignment/Script/Questions.sql
@@ -0,0 +1,31 @@
+--Ví dụ 1: Lấy tất cả các đơn hàng đã được giao trong tháng trước, cùng với thông tin về khách hàng, người nhận, và người giao hàng.
+--Người làm:
+
+
+
+
+--Ví dụ 2: Lấy danh sách các shipper đã hoàn thành hơn 66 đơn hàng, bao gồm thông tin về tổng số đơn hàng họ đã giao và danh sách các đơn hàng đó.
+--Người làm:
+
+
+--Ví dụ 3: Lấy thông tin chi tiết của các đơn hàng hiện đang "Pending", bao gồm cả khách hàng và người nhận, sắp xếp theo ngày đặt hàng mới nhất.
+--Người làm:
+
+--Ví dụ 4: Lấy ra tổng số đơn hàng đã được giao cho một khách hàng cụ thể theo mã khách hàng
+--Người làm:
+
+
+--Ví dụ 5: Lấy danh sách các đơn hàng đã bị hủy cùng với lý do hủy (nếu có), sắp xếp theo ngày đặt hàng
+--Người làm:
+
+--Ví dụ 6: Lấy thông tin về shipper có số lượng đơn hàng được giao nhiều nhất, bao gồm tên và số điện thoại của shipper
+--Người làm:
+
+--Ví dụ 7: Lấy thông tin trạng thái và tổng số lượng đơn hàng theo từng trạng thái
+--Người làm:
+
+--Ví dụ 8: Lấy danh sách khách hàng chưa đặt bất kỳ đơn hàng nào trong 6 tháng qua
+--Người làm:
+
+--Ví dụ 9: Lấy danh sách người nhận nhiều đơn hàng nhất cùng tổng số đơn đã nhận
+--Người làm:
